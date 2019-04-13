cd8cc3fcbf4f67916a65d0313c05b2415bd8c79a
jjdkq <1508540876@qq.com>
2019-04-13 19:37:16 +0800

    uboot移植

---
+++ u-boot-1.1.6/lib_arm/eabi_compat.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: lib_arm/eabi_compat.c
+	> Author: zhoubo
+	> Mail: 1508540876@qq.com 
+	> Created Time: 2018年10月31日 星期三 15时46分50秒
+ ************************************************************************/
+/*#include <common.h>
+
+int raise (int signum)
+{
+	printf("raise: Signal # %d caught\n", signum);
+	return 0;
+}	 
+void __aeabi_unwind_cpp_pr0(void)
+{
+
+};*/
